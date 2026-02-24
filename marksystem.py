6fd184e0e1a1b234a6123c90aed5d0b4456b19f5
SANA3RR-DEV <santsant3rr@gmail.com>
2026-02-24 16:46:41 +0530

    sample

---
+++ marksystem.py
@@ -0,0 +1,12 @@
+while True:
+    mark = int(input("enter mark):")
+        if mark ==-1:
+            print ("stop program")
+            break
+        if mark>=90(A):
+        else
+        if mark>=75(B):
+        else
+        if mark>=50(C):
+        else
+        
